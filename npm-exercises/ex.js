bdff59b35a91ad0ed32e633f7d755be5a690a93e
sana mahamid <sanaa_6-5.7@hotmail.com>
2020-11-18 11:09:38 +0200

    done

---
+++ npm-exercises/ex.js
@@ -0,0 +1,28 @@
+const validator = require('validator')
+ 
+
+//Ex. 1
+//Check whether "shoobert@dylan" is a valid email (should be false)
+console.log(validator.isEmail('shoobert@dylan'))
+
+//Ex. 2
+//Check whether "786-329-9958" is a valid US mobile phone number (should be true) - use the en-US locale
+console.log(validator.isMobilePhone("786-329-9958","en-US"))
+
+//Ex. 3
+//Use the following blacklist
+let blacklist = ["!", "?", ".", "@", "~", ",", "'"]
+//Along with validator's `blacklist` method to clean this text:
+let text = "I'M SO EXCITED!!!~!"
+//Ultimately, it should print "im so excited"
+console.log(validator.blacklist(text,blacklist).toLowerCase())
+
+const faker = require('faker')
+
+const makeHuman = function(num){
+    for(let i=0; i<num; i++){
+        console.log(faker.fake("{{name.firstName}}, {{image.avatar}}, {{company.companyName}}"))
+    }
+}
+
+makeHuman(2)
